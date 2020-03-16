5b1027a8b404b508da1a82640314a8941dee46d0
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-16 23:45:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,8 +2,14 @@ def hello_t
   if block_given?
     i = 0
   
-  
-  
+  while i < array.length 
+  yield(array[i])
+  i = i + 1
+end 
 
 # call your method here!
 
+array 
+elseputs "Hey! No block was given!"
+end 
+end 
